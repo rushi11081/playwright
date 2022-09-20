78aaa44f5fcf491c67d00e8d90f559a2f1e55c9c
rushikesh <rushi11081@gmail.com>
2022-09-20 15:26:44 +0530

    ss

---
+++ src/test/java/opencart/tests/Testpurpose.java
@@ -0,0 +1,13 @@
+package opencart.tests;
+
+public class Testpurpose
+{
+
+	public void get()
+	{
+		System.out.println("test");
+		System.out.println("test2");
+		System.out.println("test3");
+	}
+	
+}
